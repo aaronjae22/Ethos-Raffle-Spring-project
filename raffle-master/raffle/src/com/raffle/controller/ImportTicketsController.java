d22c6aa61ec4449dd522c5267dda0ab313a69291
Aaron Ayerdis Espinoza <aaronaej7@gmail.com>
2023-06-07 01:01:54 -0600

    4. Adding template to Import Tickets html file

--- raffle-master/raffle/src/com/raffle/controller/ImportTicketsController.java
+++ raffle-master/raffle/src/com/raffle/controller/ImportTicketsController.java
@@ -1,16 +1,45 @@
 package com.raffle.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class ImportTicketsController {
+import com.raffle.dao.UserDataAccess;
+import com.raffle.pojo.User;
 
-    @RequestMapping("import-tickets")
-    public String addTicketsFile() {
+@Controller
+class ImportTicketsController extends BaseController
+{
+
+    @Autowired
+    UserDataAccess userDataAccess;
+
+    @Secured("Administrator")
+    @RequestMapping(value ="import-tickets")
+    String viewList(Model model )
+    {
+
+        // tatuList<User> userList;
+
+        if(this.hasRole("Administrator"))
+        {
+            // userList = userDataAccess.loadUsers() ;
+            System.out.println("It has permission");
+        }
+        else
+        {
+            // userList = new ArrayList<User>();
+            System.out.println("It doesn't have permission");
+        }
+
+        // model.addAttribute("userList", userList);
 
         return "import-tickets";
-
     }
 
 }
